8a6d69eb6832c14070fe18280691ac401c0cb08e
Shailendra Kumar <vishwakarma.shailendra@gmail.com>
2018-11-15 22:42:27 +0530

    changed by user 3
    changed by user 3

--- sample-maven-project/src/main/java/com/myproject/HelloWorld.java
+++ sample-maven-project/src/main/java/com/myproject/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
 	public static void main(String arg[])
 	{
 		System.out.println("Hello World");
-		System.out.println("Changed in github");
+		System.out.println("Hello World Changed in github");
 		
 	}
 		
