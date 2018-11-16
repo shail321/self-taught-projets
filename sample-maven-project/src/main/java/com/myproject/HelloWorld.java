76daadc95a7af041f32065de437e5e3817a1b7c4
shailendra <shailendra@gmail.com>
2018-11-16 08:29:57 +0530

    changed in local repo 2

--- sample-maven-project/src/main/java/com/myproject/HelloWorld.java
+++ sample-maven-project/src/main/java/com/myproject/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld
 	{
 		System.out.println("Hello World");
 		System.out.println("Hello World Changed in github");
+		System.out.println("Hello World Changed in local-repo-2");
 		
 	}
 		
